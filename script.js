1f6278c577f2f6953821afe3f6a8b26850edf617
Dennmed22 <medrano.dennis@yahoo.com>
2023-03-30 16:15:58 -0400

    added jquery elements

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var today = moment();
+
+$('#currentDay').text(today.format('LLLL'))
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-1 .description').val(localStorage.getItem('hour-1'));
+$('#hour-2 .description').val(localStorage.getItem('hour-2'));
+$('#hour-3 .description').val(localStorage.getItem('hour-3'));
+$('#hour-4 .description').val(localStorage.getItem('hour-4'));
+$('#hour-5 .description').val(localStorage.getItem('hour-5'));
+
+
+$(document).ready(function () {
+  $('div').wrap('<div class="containerDiv"></div>');
+
+  $('.saveBtn').on('click', function () {
+    const textValue = $(this).siblings('.description').val();
+    const timeKey = $(this).parent().attr('id');
+    localStorage.setItem(timeKey, textValue);
+
+  });
+
+});
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +48,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
